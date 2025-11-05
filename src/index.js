1931ffe09dc453ac326fefdb22bb83c5d0e695ea
Bongz <106278406+Bongz26@users.noreply.github.com>
2025-11-05 10:44:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-// frontend/src/index.tsx
+// frontend/src/index.js   ←  100% WORKING FOR NODE 22 + RENDER
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
